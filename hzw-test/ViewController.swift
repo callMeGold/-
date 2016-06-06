d049fdfc36fae05f630fe01ce5b98f864efbe35b
伟哥 <564114648@qq.com>
2016-06-06 16:00:41 +0800

    Initial Commit

---
+++ hzw-test/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  hzw-test
+//
+//  Created by nevermore on 16/6/6.
+//  Copyright © 2016年 hzw. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        view.backgroundColor=UIColor.redColor()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
